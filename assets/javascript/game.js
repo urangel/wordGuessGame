ca9269d8c86d25ac267d17a19ebcc9c4bc2767cf
Ulises Rangel <uli.atx@gmail.com>
2019-03-05 11:52:52 -0600

    more scripting and functionality added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,15 @@ var alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
 
 //creats an array consisting of the letters of the mystery word that was chosen at random from the list
 var mysteryWord = (austinThings[Math.floor(Math.random()*austinThings.length)]).split("");
+console.log(mysteryWord);
+
+var uniqueLetters = mysteryWord.filter(function(item, pos){
+    return mysteryWord.indexOf(item) == pos;
+})
+
+console.log(uniqueLetters);
+
+var uniqueLower = [];
 
 var guessesLeft = 12;
 
@@ -34,45 +43,87 @@ function winsIncrease() {
     wins += 1;
 }
 
+function restart() {
+    mysteryWord = (austinThings[Math.floor(Math.random()*austinThings.length)]).split("");
+    console.log(mysteryWord);
+    guessesLeft = 12;
+    lettersCorrect = [];
+    lettersWrong = [];
+    document.getElementById("mysteryWord").textContent = "";
+    document.getElementById("lettersGuessed").textContent = "";
+    document.getElementById("guessesLeft").textContent = guessesLeft;
+    document.getElementById("status").textContent = "Guess what Austin thing this is!";
+    alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
+    
+    uniqueLetters = mysteryWord.filter(function(item, pos){
+        return mysteryWord.indexOf(item) == pos;
+    })
+    
+    for (i=0; i<uniqueLetters.length; i++){
+        uniqueLower.push(uniqueLetters[i].toLowerCase());
+    }
+    
+    console.log(uniqueLower);
+    
+}
+
+// setting up so that word is only lowercase
+
+for (i=0; i<uniqueLetters.length; i++){
+    uniqueLower.push(uniqueLetters[i].toLowerCase());
+}
+
+console.log(uniqueLower);
+
 // The beginning of user interactivity
 
 document.onkeyup = function(event){
     var userGuess = event.key;
     
+    if (uniqueLower.length >= 1){ //if all of the letters in the word have not been guessed... then play
 
-    if (alphabet.indexOf(userGuess) >= 0){
-        alphabet.splice(alphabet.indexOf(userGuess), 1 );
-    
-        if (mysteryWord.indexOf(userGuess) >= 0){
-            // add the user guessed letter *that is correct* to the array lettersCorrect
-            for (k=0; k<mysteryWord.length; k++){
-                if (userGuess === mysteryWord[k])
+        if (guessesLeft > 1){
+            if (alphabet.indexOf(userGuess) >= 0){
+                alphabet.splice(alphabet.indexOf(userGuess), 1 );
+            
+                if (uniqueLower.indexOf(userGuess) >= 0){
+                    // lettersCorrect.push(userGuess); // Need to change this to make the word appear in order so it makes sense
+                    // document.getElementById("mysteryWord").textContent = lettersCorrect;
+                    $("#mysteryWord").html("<p>" + userGuess + "</p>");
+                        if (uniqueLower.indexOf(userGuess) >=0){
+                        uniqueLower.splice(uniqueLetters.indexOf(userGuess), 1);
+                        }    
+                    console.log(uniqueLower);
+                }
+                else {
+                // adds the user guessed letter *that is wrong* to the array lettersWrong
+                lettersWrong.push(userGuess);
+                document.getElementById("lettersGuessed").textContent = lettersWrong;
+                // gives you one less try because you guessed wrong
+                guessesDecrease();
+                document.getElementById("guessesLeft").textContent = guessesLeft;
+                }
+
             }
-            lettersCorrect.push(userGuess);
-            document.getElementById("mysteryWord").textContent = lettersCorrect;
-        }
-        else {
-          // adds the user guessed letter *that is wrong* to the array lettersWrong
-          lettersWrong.push(userGuess);
-          document.getElementById("lettersGuessed").textContent = lettersWrong;
-          // gives you one less try because you guessed wrong
-          guessesDecrease();
-          document.getElementById("guessesLeft").textContent = guessesLeft;
+            else {
+                alert("Please choose a letter in the alphabet that you haven't already chosen");
+            }
         }
 
+        else {
+            guessesDecrease();
+            document.getElementById("guessesLeft").textContent = guessesLeft;
+            document.getElementById("status").textContent = "Game Over!";
+            restart();
+        }
     }
     else {
-        alert("Please choose a letter in the alphabet that you haven't already chosen")
-    }
-
-
-       
+        document.getElementById("status").textContent = "You got it!";
+        winsIncrease();
+        document.getElementById("wins").textContent = wins;
+        restart();
+    }    
 }
-
+//if all of the letters in the mysteryword are guessed then the user wins...
 // need to populate all instances of correct letter if it apppears multiple times in the mysteryWord
-
-
-//defining the userGuess variable to take the key that was pressed
-document.onkeyup(event){
-    var userGuess = event.key;
-}
+// need to put all correct guesses in the right order to form the word
