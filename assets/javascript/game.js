967553be45effe674994a3ee2645135c3456565b
Ulises Rangel <uli.atx@gmail.com>
2019-03-05 05:05:05 -0600

    javascript event functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,10 @@
 var austinThings = ["mountBonnel", "wholeFoods", "bartonSprings", "heb", "grackles", "amysIceCream", "acl", "zilkerPark", "sxsw", "torchiesTacos", "ladyBirdLake", "congressBridge", "stateCapitol"];
 
-//defining the userGuess variable to take the key that was pressed
-document.onkeyup(event){
-    var userGuess = event.key;
-}
-
 //creating an array that consists of the letters of the english alphabet
 var alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
+
+//creats an array consisting of the letters of the mystery word that was chosen at random from the list
+var mysteryWord = (austinThings[Math.floor(Math.random()*austinThings.length)]).split("");
 
 var guessesLeft = 12;
 
@@ -16,22 +14,65 @@ var lettersWrong = [];
 
 var wins = 0;
 
-// The beginning of user interactivity
-for (i=0; guessesLeft > 0; i++){
-    for (j=0; j<austinThings[i]; j++){
-        if (userGuess === austinThings[j]){
-            // add the user guessed letter *that is correct* to the array lettersCorrect
-            lettersCorrect.push(userGuess);
-        }
-        else {
-            // adds the user guessed letter *that is wrong* to the array lettersWrong
-            lettersWrong.push(userGuess);
-            // gives you one less try because you guessed wrong
-            guessesLeft - 1;
-            document.getElementById("guessesLeft").textContent = guessesLeft;
-        }
-    }
+// when the document loads...
+
+window.onload = function() {
+    document.getElementById("guessesLeft").textContent = guessesLeft;
+    document.getElementById("wins").textContent = wins;
+
 }
 
 
-// document.getElementById("cpuPick").textContent = "Computer chooses: " + cpu;
+// some functions
+
+function guessesDecrease() {
+    guessesLeft -= 1;
+}
+
+
+function winsIncrease() {
+    wins += 1;
+}
+
+// The beginning of user interactivity
+
+document.onkeyup = function(event){
+    var userGuess = event.key;
+    
+
+    if (alphabet.indexOf(userGuess) >= 0){
+        alphabet.splice(alphabet.indexOf(userGuess), 1 );
+    
+        if (mysteryWord.indexOf(userGuess) >= 0){
+            // add the user guessed letter *that is correct* to the array lettersCorrect
+            for (k=0; k<mysteryWord.length; k++){
+                if (userGuess === mysteryWord[k])
+            }
+            lettersCorrect.push(userGuess);
+            document.getElementById("mysteryWord").textContent = lettersCorrect;
+        }
+        else {
+          // adds the user guessed letter *that is wrong* to the array lettersWrong
+          lettersWrong.push(userGuess);
+          document.getElementById("lettersGuessed").textContent = lettersWrong;
+          // gives you one less try because you guessed wrong
+          guessesDecrease();
+          document.getElementById("guessesLeft").textContent = guessesLeft;
+        }
+
+    }
+    else {
+        alert("Please choose a letter in the alphabet that you haven't already chosen")
+    }
+
+
+       
+}
+
+// need to populate all instances of correct letter if it apppears multiple times in the mysteryWord
+
+
+//defining the userGuess variable to take the key that was pressed
+document.onkeyup(event){
+    var userGuess = event.key;
+}
